f65f571b37c720f90ed61fbc9973e4622ae56053
It's Fer <fernandabelokurows@gmail.com>
2026-04-06 00:31:03 +0000

    Commit

--- 44. Atividade: Xadrez.c
+++ 44. Atividade: Xadrez.c
@@ -1,5 +1,66 @@
 #include <stdio.h>
 
+    /*********** Movimentação da Torre ***********/
+    // Move-se em linha reta horizontalmente ou verticalmente. Deve movimentar-se cinco casas para a direita.
+    // Se peça = 1-Torre
+
+void movimenta_torre(int casas, int *movimentos) {
+    // Para finalizar a função, quando terminar de rodar
+    if (casas <= 0) {
+    return;
+    }
+
+    // Imprime dados e soma a quantidade de movimentos
+    printf("Direita\n");
+    *movimentos += 1; 
+
+    // Chamada recursiva
+    movimenta_torre(casas-1, movimentos); 
+
+}
+
+    /*********** Movimentação do Bispo ***********/
+    // Move-se na diagonal. Deve movimentar-se cinco casas na diagonal para cima e à direita.
+    // Se peça = 2-Bispo
+
+void movimenta_bispo(int casas_cima, int casas_direita, int *movimentos) {
+    // Para finalizar a função, quando terminar de rodar
+    if (casas_cima <= 0 && casas_direita <= 0) {
+    return;
+    }
+
+    // Imprime dados e soma a quantidade de movimentos
+    printf("Cima\n");
+    printf("Direita\n");
+    *movimentos += 2; 
+
+    // Chamada recursiva
+    movimenta_bispo(casas_cima-1, casas_direita-1, movimentos); 
+
+}
+
+    /*********** Movimentação da Rainha ***********/
+    // Move-se em todas as direções. Deve movimentar-se oito casas para a esquerda.
+    // Se peça = 3-Rainha
+
+void movimenta_rainha(int casas, int *movimentos) {
+    // Para finalizar a função, quando terminar de rodar
+    if (casas <= 0) {
+    return;
+    }
+
+    // Imprime dados e soma a quantidade de movimentos
+    printf("Esquerda\n");
+    *movimentos += 1; 
+
+    // Chamada recursiva
+    movimenta_rainha(casas-1, movimentos); 
+
+}
+
+
+
+
 int main() {
 
     /*********** Variáveis ***********/
@@ -18,7 +79,7 @@ int main() {
         printf("3-Rainha\n");
         printf("4-Cavalo\n");
         printf("5-Finalizar\n");        
-        printf("Informe a peça a ser movimentada: ");
+        printf("Informe a opção desejada: ");
         scanf("%d", &peca);
 
     /*********** Movimentação da Torre ***********/
@@ -28,16 +89,9 @@ int main() {
         if (peca == 1) {
             printf("Peça escolhida: 1-Torre\n");
 
-            // Reinicia a variável como a primeira casa a ser movimentada
-            casas_movimentar = 1;
+            // Função recursiva
             movimentos = 0;
-
-            // Movimento com While
-            while (casas_movimentar <= 5) {   
-            printf("Direita\n");
-            casas_movimentar++;
-            movimentos++; 
-            }
+            movimenta_torre(5, &movimentos); 
 
             // Total de casas movimentadas
             casas_total += movimentos;
@@ -51,22 +105,13 @@ int main() {
         if (peca == 2) {
             printf("Peça escolhida: 2-Bispo\n");
 
-            // Reinicia a variável como a primeira casa a ser movimentada
-            casas_movimentar = 1;
+            // Função recursiva
             movimentos = 0;
-
-            // Movimento com Do-While
-            do {
-            printf("Direita\n");
-            printf("Cima\n");       
-            casas_movimentar++;
-            movimentos++;
-            movimentos++; 
-            } while (casas_movimentar <= 5);
+            movimenta_bispo(5, 5, &movimentos); 
 
             // Total de casas movimentadas
             casas_total += movimentos;
-            printf("O bispo andou %d casas para a direita e para cima, sendo %d na diagonal.\n", movimentos, movimentos/2);
+            printf("O bispo andou %d casas para a direita e para cima, ou seja, %d na diagonal.\n", movimentos, movimentos/2);
         }
 
 
@@ -80,12 +125,9 @@ int main() {
             // Reinicia a variável como a primeira casa a ser movimentada
             movimentos = 0;
 
-            // Movimento com For
-            for (int casas_movimentar = 1; casas_movimentar <= 8; casas_movimentar++) 
-            {
-            printf("Esquerda\n");
-            movimentos++; 
-            }   
+            // Função recursiva
+            movimentos = 0;
+            movimenta_rainha(8, &movimentos); 
 
             // Total de casas movimentadas
             casas_total += movimentos;
@@ -93,7 +135,7 @@ int main() {
         }
 
     /*********** Movimentação do Cavalo ***********/
-    // Move-se em L. Deve movimentar-se duas casas para baixo e uma casa para a esquerda.
+    // Move-se em L. Deve movimentar-se duas casas para cima e uma casa para a direita.
     // Se peça = 4-Cavalo
 
         if (peca == 4) {
@@ -103,35 +145,36 @@ int main() {
             movimentos = 0;
 
             // Movimento com Loop Aninhado For
-            int casas_baixo = 0;
+            int casas_cima = 0;
 
-            for (int casas_movimentar = 1; casas_movimentar <= 2; casas_movimentar++) 
-            {
+            for (int casas_movimentar = 1; casas_movimentar <= 2; casas_movimentar++)             {
                 for (int casas_mover = 1; casas_mover <= 1; casas_mover++) 
                 {
-                    printf("Baixo\n");
+                    printf("Cima\n");
                     movimentos++; 
                 }
-                casas_baixo = casas_movimentar;
+                casas_cima = casas_movimentar;
             }
         
             // Movimento com Loop Aninhado While
             casas_movimentar = 1;
-            int casas_esquerda = 1;
+            int casas_direita_2 = 0;    
+            int casas_direita = 1;
 
             while (casas_movimentar <= 1) {
             casas_movimentar++;
-                while (casas_esquerda <= 1) 
+                while (casas_direita <= 1) 
                 {
-                    casas_esquerda++;
-                    printf("Esquerda\n");
+                    casas_direita++;
+                    casas_direita_2+=1;
+                    printf("Direita\n");
                     movimentos++; 
                 }
             }
 
             // Total de casas movimentadas
             casas_total += movimentos;
-            printf("O cavalo andou %d casas para baixo e %d para a esquerda, sendo %d no total.\n", casas_baixo, casas_esquerda, movimentos);
+            printf("O cavalo andou %d casas para cima e %d para a direita, sendo %d no total.\n", casas_cima, casas_direita_2, movimentos);
 
         }
 
